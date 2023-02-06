de1f5b58186da31f9588463b9a70e9b6a890894b
태쩡이 <81836250+ktj001004@users.noreply.github.com>
2023-02-06 00:32:56 +0000

    2.10 Recap II 과제

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Chrome_App
-바닐라 JS로 크롬 앱 만들기 - NomadCoder
+# 바닐라 JS로 크롬 앱 만들기!
+- JavaScript 입문 코스
+- by NomadCoder (노마드 코더) (https://nomadcoders.co/javascript-for-beginners)
---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator= {
+    add: function(a, b){
+      console.log(a + b);
+    },
+    minus: function(a, b){
+      console.log(a - b);
+    },
+    divide: function(a, b){
+      console.log(a / b);
+    },
+    powerof: function(a, b){
+      console.log(a ** b);
+    },
+  };
+  
+  calculator.add(1, 2);
+  calculator.minus(1, 2);
+  calculator.divide(1, 2);
+  calculator.powerof(1, 2);
