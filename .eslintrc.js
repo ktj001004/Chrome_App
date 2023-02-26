5700368c8e77621058786093048b90699395e6e2
Tae Jeong <ktj332211@naver.com>
2023-02-26 23:36:09 +0900

    eventListener

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
   root: true,
   plugins: ["@typescript-eslint"],
-  extends: [
-    "plugin:prettier/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   rules: {
-    "@typescript-eslint/strict-boolean-expressions": [
-      2,
-      {
-        allowString: false,
-        allowNumber: false,
-      },
-    ],
+    "@typescript-eslint/strict-boolean-expressions": 0,
+  },
+  env: {
+    browser: true,
+    node: true,
   },
 };
