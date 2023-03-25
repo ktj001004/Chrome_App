f3582217fd2bef8d6251c32b7bb693c9d94408ec
Tae Jeong <ktj332211@naver.com>
2023-03-25 23:10:52 +0900

    JavaScript On The Browser

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
+  h1.classList.toggle("clicked");
 }
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-  alert("copied!");
-}
-function handleWindowOffline() {
-  alert("SOS no WIFI!");
-}
-function handleWindowOnline() {
-  alert("All Good!");
-}
-
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
